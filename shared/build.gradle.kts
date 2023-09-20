36bd4bb3317666f7885b00d4dff58520f9eeb7af
FacuSosa <facusosa1607@gmail.com>
2023-09-19 22:36:54 -0300

    Integrando dependencias de ktor y serialization

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -26,9 +26,17 @@ kotlin {
     }
 
     sourceSets {
+        val ktorVersion = "2.0.0-beta-1"
+
         val commonMain by getting {
             dependencies {
-                //put your multiplatform dependencies here
+                //KTOR
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+
+                //SERIALIZATION
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+
             }
         }
         val commonTest by getting {
@@ -36,6 +44,22 @@ kotlin {
                 implementation(kotlin("test"))
             }
         }
+        val androidMain by getting {
+            dependencies {
+                //KTOR
+                implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
+
+            }
+        }
+
+        val iosMain by getting {
+            dependencies {
+                //KTOR
+                implementation("io.ktor:ktor-client-ios:$ktorVersion")
+
+            }
+        }
+
     }
 }
 
